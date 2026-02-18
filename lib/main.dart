b7c73a2bae9f66765c1e5500735c6289b3774ff2
ihthisham <your-ihthishampno19@gmail.com>
2026-02-18 14:12:49 +0530

    authUIFirebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:foodiex/features/auth/login/presentation/screens/login_screen.dart';
+import 'package:foodiex/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+if (!kIsWeb && !Platform.isLinux) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
   runApp(const MyApp());
 }
 
